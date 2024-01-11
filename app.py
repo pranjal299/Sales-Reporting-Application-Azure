cf15b55ae5bcc0d7d7c59f66b77b4df9876516cd
Pranjal Ranjan <pranjalranjan@vt.edu>
2024-01-11 13:27:05 +0530

    Selects container according to environment now

--- app.py
+++ app.py
@@ -6,7 +6,10 @@ app = Flask(__name__)
 
 # Azure Storage Account settings
 CONNECTION_STRING = 'DefaultEndpointsProtocol=https;AccountName=salesrepstorageacc;AccountKey=7VFqRFCzw+o5Dg4iTmFcClNkhDndSTNGKE1YcdFlVhxbrWP/q0JWOS4VDZvgVezQLcw8WK4KEj7I+ASts2Kh/A==;EndpointSuffix=core.windows.net'
-CONTAINER_NAME = 'salesrepcontainer'
+
+# Determine the environment and set the container name
+ENVIRONMENT = os.getenv('FLASK_ENV', 'development')
+CONTAINER_NAME = 'salesrepcontainerlocal' if ENVIRONMENT == 'development' else 'salesrepcontainer'
 
 blob_service_client = BlobServiceClient.from_connection_string(CONNECTION_STRING)
 
@@ -22,7 +25,7 @@ def upload_files_to_blob():
     for file in uploaded_files:
         if file:
             blob_client = blob_service_client.get_blob_client(container=CONTAINER_NAME, blob=file.filename)
-            blob_client.upload_blob(file)
+            blob_client.upload_blob(file, overwrite=True)
             messages.append(f'Successfully uploaded {file.filename}!')
         else:
             messages.append('A required file was missing.')
